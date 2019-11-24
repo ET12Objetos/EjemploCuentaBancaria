3adb37fa6384bdf588a703ff2cb02eb672904801
jvelazquez <jonathanvgms@gmail.com>
2019-11-24 13:58:27 -0300

    Add Program.cs

---
+++ Ejercicio1/CuentaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class CuentaBancaria
+    {
+        public int nroCuenta;
+        public int nroCliente;
+        public string moneda;
+        public double saldo;
+
+        public void depositarCantidad(double dinero)
+        {
+            validarDinero(dinero);            
+            saldo = saldo + dinero;
+        }
+
+        private void validarDinero(double dinero)
+        {
+            if (dinero < 0)
+                throw new Exception("Error, monto dinero incorrecto");
+        }
+
+        public void informarEstado()
+        {
+            Console.WriteLine("*****************************************");
+            Console.WriteLine($"Nro Cuenta: {nroCuenta} Nro Cliente: {nroCliente} Moneda: {moneda} Saldo: {saldo}");
+            Console.WriteLine("*****************************************");
+        }
+
+        public void retirarCantidad(double dinero)
+        {
+            validarDinero(dinero);
+            double saldoOperacion = saldo - dinero;
+            if (saldoOperacion < 0)
+                throw new Exception("Fondos insuficientes");
+            saldo = saldoOperacion;
+        }
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaBancaria unaCuenta = new CuentaBancaria();
+            Console.WriteLine("Ingrese Nro Cuenta Bancaria:");
+            unaCuenta.nroCuenta = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese Nro Cliente:");
+            unaCuenta.nroCliente = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese Moneda:");
+            unaCuenta.moneda = Console.ReadLine();
+            Console.WriteLine("Ingrese Saldo:");
+            double unSaldo = Convert.ToDouble(Console.ReadLine());
+            if (unSaldo < 0)
+            {
+                Console.WriteLine("Saldo cuenta incorrecto");
+                return;
+            }
+            unaCuenta.saldo = unSaldo;
+            Console.WriteLine("Ingrese cantidad dinero a depositar:");
+            double dinero = Convert.ToDouble(Console.ReadLine());
+            unaCuenta.depositarCantidad(dinero);
+            unaCuenta.informarEstado();
+            Console.WriteLine("Ingrese cantidad dinero a retirar:");
+            dinero = Convert.ToDouble(Console.ReadLine());
+            unaCuenta.retirarCantidad(dinero);
+            unaCuenta.informarEstado();
+        }
+    }
+}
